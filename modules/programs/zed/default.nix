c5d1edbf0e5514307e2aac8d23457060469697bc
Jake Woods <jake@jakewoods.net>
2026-07-25 13:05:56 +1000

    add zed

---
+++ modules/programs/zed/default.nix
@@ -0,0 +1,22 @@
+{ self, inputs, ... }:
+
+{
+  flake.aspects = { aspects, ... }: {
+    zed = {
+      description = "Zed Editor";
+
+      homeManager = { config, pkgs, ... }: {
+        # We don't use the home-manager package since we're using
+        # our mutableInputs module to manage settings anyway.
+        home.packages = [
+          (pkgs.zed-editor.fhsWithPackages (pkgs: [ ]))
+        ];
+
+        xdg.configFile."zed/settings.json".source =
+          config.lib.mutableInputs.link ./settings.json;
+        xdg.configFile."zed/keymap.json".source =
+          config.lib.mutableInputs.link ./keymap.json;
+      };
+    };
+  };
+}
