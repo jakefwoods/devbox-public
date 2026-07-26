b4edc0275ae8ac90adefafe824117477e9ad2a72
Jake Woods <jake@jakewoods.net>
2026-07-26 16:41:25 +1000

    zed: nix support

--- modules/programs/zed/default.nix
+++ modules/programs/zed/default.nix
@@ -1,22 +1,26 @@
 { self, inputs, ... }:
 
 {
-  flake.aspects = { aspects, ... }: {
-    zed = {
-      description = "Zed Editor";
+  flake.aspects =
+    { aspects, ... }:
+    {
+      zed = {
+        description = "Zed Editor";
 
-      homeManager = { config, pkgs, ... }: {
-        # We don't use the home-manager package since we're using
-        # our mutableInputs module to manage settings anyway.
-        home.packages = [
-          (pkgs.zed-editor.fhsWithPackages (pkgs: [ ]))
-        ];
+        homeManager =
+          { config, pkgs, ... }:
+          {
+            # We don't use the home-manager package since we're using
+            # our mutableInputs module to manage settings anyway.
+            home.packages = [
+              (pkgs.zed-editor.fhsWithPackages (pkgs: [
+                pkgs.nixd
+              ]))
+            ];
 
-        xdg.configFile."zed/settings.json".source =
-          config.lib.mutableInputs.link ./settings.json;
-        xdg.configFile."zed/keymap.json".source =
-          config.lib.mutableInputs.link ./keymap.json;
+            xdg.configFile."zed/settings.json".source = config.lib.mutableInputs.link ./settings.json;
+            xdg.configFile."zed/keymap.json".source = config.lib.mutableInputs.link ./keymap.json;
+          };
       };
     };
-  };
 }
