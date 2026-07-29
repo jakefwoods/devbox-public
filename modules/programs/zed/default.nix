967fba0eaf48aec0de48652a3762ad75da1ecc24
Jake Woods <jake@jakewoods.net>
2026-07-29 10:31:13 +1000

    Update zed darwin support

--- modules/programs/zed/default.nix
+++ modules/programs/zed/default.nix
@@ -12,12 +12,18 @@
           {
             # We don't use the home-manager package since we're using
             # our mutableInputs module to manage settings anyway.
-            home.packages = [
-              (pkgs.zed-editor.fhsWithPackages (pkgs: [
-                pkgs.nixd
-              ]))
-            ];
-
+            home.packages = [] 
+              ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [ 
+                   (pkgs.zed-editor.fhsWithPackages (pkgs: [
+                     pkgs.nixd
+                   ]))
+                 ])
+              ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [ 
+                   # no fhs wrap on darwin
+                   pkgs.zed-editor
+		   pkgs.nixd
+                 ]);
+   
             xdg.configFile."zed/settings.json".source = config.lib.mutableInputs.link ./settings.json;
             xdg.configFile."zed/keymap.json".source = config.lib.mutableInputs.link ./keymap.json;
           };
